28cd501b3fc824eb8c0941b5b73a3ee4b74e9d38
Oliver Lemke <olemke@gmail.com>
2019-03-19 12:57:16 +0100

    Fix reshaping.

--- convert2nc.py
+++ convert2nc.py
@@ -9,8 +9,8 @@ from netCDF4 import Dataset
 
 def convert_arts_to_nc(casename):
     fluxes = axml.load(f'in/{casename}/fluxes.{casename}.level_input.xml')
-    fluxes = np.array(fluxes).reshape(100, 18, 61, 3)
-    fluxes = np.swapaxes(fluxes, 0, 1)
+    fluxes = np.array(fluxes).reshape(18, 100, 61, 3)
+    #fluxes = np.swapaxes(fluxes, 0, 1)
     fluxes = np.flip(fluxes, 2)
 
     shutil.copy('template/rld_Efx_ARTS-2-3_rad-irf_r1i1p1f1_gn.nc',
@@ -20,7 +20,7 @@ def convert_arts_to_nc(casename):
 
     with Dataset(f'out/rld_Efx_ARTS-2-3_rad-irf_r1i1p1f1_gn_{casename}.nc',
                  'a') as ds:
-        ds['rld'][:] = fluxes[:, :, :, 2]
+        ds['rld'][:] = -fluxes[:, :, :, 2]
     with Dataset(f'out/rlu_Efx_ARTS-2-3_rad-irf_r1i1p1f1_gn_{casename}.nc',
                  'a') as ds:
         ds['rlu'][:] = fluxes[:, :, :, 1]

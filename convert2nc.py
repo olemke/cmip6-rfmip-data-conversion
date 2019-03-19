9e75ee1fde87db271d24db460d8f2e05f3e063b9
Oliver Lemke <olemke@gmail.com>
2019-03-19 14:20:24 +0100

    Use physics_index 2 for LM

--- convert2nc.py
+++ convert2nc.py
@@ -7,10 +7,9 @@ import typhon.arts.xml as axml
 from netCDF4 import Dataset
 
 
-def convert_arts_to_nc(casename):
+def convert_arts_to_nc(casename, physics_index=1):
     fluxes = axml.load(f'in/{casename}/fluxes.{casename}.level_input.xml')
     fluxes = np.array(fluxes).reshape(18, 100, 61, 3)
-    #fluxes = np.swapaxes(fluxes, 0, 1)
     fluxes = np.flip(fluxes, 2)
 
     shutil.copy('template/rld_Efx_ARTS-2-3_rad-irf_r1i1p1f1_gn.nc',
@@ -21,11 +20,14 @@ def convert_arts_to_nc(casename):
     with Dataset(f'out/rld_Efx_ARTS-2-3_rad-irf_r1i1p1f1_gn_{casename}.nc',
                  'a') as ds:
         ds['rld'][:] = -fluxes[:, :, :, 2]
+        ds.physics_index = np.int32(physics_index)
+
     with Dataset(f'out/rlu_Efx_ARTS-2-3_rad-irf_r1i1p1f1_gn_{casename}.nc',
                  'a') as ds:
         ds['rlu'][:] = fluxes[:, :, :, 1]
+        ds.physics_index = np.int32(physics_index)
 
 
 if __name__ == '__main__':
-    convert_arts_to_nc('allCFCs_LM')
     convert_arts_to_nc('allCFCs')
+    convert_arts_to_nc('allCFCs_LM', physics_index=2)

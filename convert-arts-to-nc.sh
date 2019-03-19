f2636032fa1dd6bf0367836c86da2eb9ca0196d0
Oliver Lemke <olemke@gmail.com>
2019-03-19 09:21:05 +0100

    Generate outputs from two configs.

--- convert-arts-to-nc.sh
+++ convert-arts-to-nc.sh
@@ -2,12 +2,14 @@
 
 [[ ! -d out ]] && mkdir out
 [[ ! -d out ]] && exit 1
+[[ ! -d template ]] && mkdir template
+[[ ! -d template ]] && exit 1
 
 ln -sf in/atmdata/multiple_input4MIPs_radiation_RFMIP_UColorado-RFMIP-1-1_none.nc
 
 ./generate-output-file-templates.py --source_id ARTS-2-3 
 rm -f rs[du]_Efx_ARTS-2-3_rad-irf_r1i1p1f1_gn.nc
-mv rl[du]_Efx_ARTS-2-3_rad-irf_r1i1p1f1_gn.nc out/
+mv rl[du]_Efx_ARTS-2-3_rad-irf_r1i1p1f1_gn.nc template/
 
 python convert2nc.py
 
--- convert2nc.py
+++ convert2nc.py
@@ -1,21 +1,31 @@
 # /usr/bin/env python3
 
+import shutil
+
 import numpy as np
 import typhon.arts.xml as axml
 from netCDF4 import Dataset
 
 
-def convert_arts_to_nc():
-    fluxes = axml.load('in/allCFCs/fluxes.allCFCs.level_input.xml')
+def convert_arts_to_nc(casename):
+    fluxes = axml.load(f'in/{casename}/fluxes.{casename}.level_input.xml')
     fluxes = np.array(fluxes).reshape(100, 18, 61, 3)
     fluxes = np.swapaxes(fluxes, 0, 1)
     fluxes = np.flip(fluxes, 2)
 
-    with Dataset('out/rld_Efx_ARTS-2-3_rad-irf_r1i1p1f1_gn.nc', 'a') as ds:
+    shutil.copy('template/rld_Efx_ARTS-2-3_rad-irf_r1i1p1f1_gn.nc',
+                f'out/rld_Efx_ARTS-2-3_rad-irf_r1i1p1f1_gn_{casename}.nc')
+    shutil.copy('template/rlu_Efx_ARTS-2-3_rad-irf_r1i1p1f1_gn.nc',
+                f'out/rlu_Efx_ARTS-2-3_rad-irf_r1i1p1f1_gn_{casename}.nc')
+
+    with Dataset(f'out/rld_Efx_ARTS-2-3_rad-irf_r1i1p1f1_gn_{casename}.nc',
+                 'a') as ds:
         ds['rld'][:] = fluxes[:, :, :, 2]
-    with Dataset('out/rlu_Efx_ARTS-2-3_rad-irf_r1i1p1f1_gn.nc', 'a') as ds:
+    with Dataset(f'out/rlu_Efx_ARTS-2-3_rad-irf_r1i1p1f1_gn_{casename}.nc',
+                 'a') as ds:
         ds['rlu'][:] = fluxes[:, :, :, 1]
 
 
 if __name__ == '__main__':
-    convert_arts_to_nc()
+    convert_arts_to_nc('default')
+    convert_arts_to_nc('allCFCs')

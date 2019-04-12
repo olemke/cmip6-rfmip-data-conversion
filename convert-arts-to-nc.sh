016e66f7e751efeec8ad73a09c03231558b0a4f2
Oliver Lemke <olemke@gmail.com>
2019-04-12 07:32:12 +0200

    Update input4MIPS

--- convert-arts-to-nc.sh
+++ convert-arts-to-nc.sh
@@ -3,7 +3,7 @@
 [[ ! -d out ]] && mkdir out
 [[ ! -d out ]] && exit 1
 
-ln -sf in/atmdata/multiple_input4MIPs_radiation_RFMIP_UColorado-RFMIP-1-1_none.nc
+ln -sf in/atmdata/multiple_input4MIPs_radiation_RFMIP_UColorado-RFMIP-1-2_none.nc
 
 ./generate-output-file-templates.py --physics_index=1 --source_id ARTS-2-3
 ./generate-output-file-templates.py --physics_index=2 --source_id ARTS-2-3
